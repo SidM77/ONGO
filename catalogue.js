c54e691cd692b333bfd591a90fa2a5d1924f2abc
SidM77 <102024701+SidM77@users.noreply.github.com>
2022-12-23 02:25:51 +0530

    Add files via upload

---
+++ catalogue.js
@@ -0,0 +1,393 @@
+
+// body part
+var prounder15data = [
+
+  
+
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/assets/images/syngonium_podophyllum.jpg",
+      proname: "Non Flowering",
+      prosoiltype:"Clayey-Soil",
+      maintenance:"Easy",
+      prorate:"Arrowhead ₹150",
+      pronotes: "Very similar species as the Philodendron and from the same family.",
+      rate: 150,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/aluminum_plant.jpg",
+      proname: "Non Flowering",
+      prosoiltype:"Loamy-Soil",
+      prorate:"Aluminum Plant ₹220",
+      maintenance:"Moderate",
+      pronotes: "An easy going house plant that is generally simple to please.",
+      rate: 220,
+  
+    },
+  
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/winter-cherry-plant.jpg",
+      proname: "Flowering",
+      prosoiltype:"Black-Soil",
+      prorate:"Winter Cherry-₹460",
+      maintenance:"Easy",
+      pronotes: "You will find it's a popular ornamental plant to buy at Christmas time.",
+      rate: 460,
+  
+    },
+  
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/close-up-urn-plant-flower.jpg",
+      proname: "Flowering",
+      prosoiltype:"Red-Soil",
+      prorate:"Urn Plant ₹190",
+      maintenance:"Easy",
+      pronotes: "The Urn plant is also known as the silver vase plant",
+      rate: 190,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/canary.jpg",
+      proname: "Non Flowering",
+      prosoiltype:"Marshy-Soil",
+      prorate:"Canary Palm ₹300",
+      maintenance:"Moderate",
+      pronotes: "A tough palm displaying long fronds with thin straight leaflets.",
+      rate: 300,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/assets/images/gerbera_jamesonii_1.jpg",
+      proname: "Flowering",
+      prosoiltype:"Marshy-Soil",
+      prorate:"Gerbera ₹400",
+      maintenance:"Very Difficult",
+      pronotes: "A flowering pot plant displaying striking flowers.",
+      rate: 400,
+  
+    },
+    {
+         
+      proimg: "http://cdn.shopify.com/s/files/1/0255/9760/8011/products/71872-00-BAKIE.jpg?v=1650502785",
+      proname: "Flowering",
+      prosoiltype:"Red-Soil",
+      prorate:"Flamingo Flower ₹700",
+      maintenance:"Difficult",
+      pronotes: "An always lush looking plant with a striking flowers.",
+      rate: 700,
+  
+    },
+    {
+         
+      proimg: "https://www.thespruce.com/thmb/wdW-q3vtH4z_K8ycblqdkieWAVE=/2000x2000/smart/filters:no_upscale()/dumb-cane-dieffenbachia-definition-1902751-5-a552c54030764900b2b17975efab5ff4.jpg",
+      proname: "Non Flowering",
+      prosoiltype:"Clayey Soil",
+      prorate:"Dumb Cane ₹240",
+      maintenance:"Easy",
+      pronotes: "Its easy to care for style make it a wonderful house plant.",
+      rate: 240,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/Clivia-Miniata.jpg",
+      proname: "Flowering",
+      prosoiltype:"Loamy-Soil",
+      prorate:"Kaffir Lily ₹620",
+      maintenance:"Difficult",
+      pronotes: "A plant exhibiting the lovely colours of summer-orange and oink",
+      rate: 620,
+  
+    },
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/aglaonema.png",
+      proname: "Non Flowering",
+      prosoiltype:"Sandy-Soil",
+      prorate:"Chinese Evergreen ₹360",
+      maintenance:"Easy",
+      pronotes: "Grown primarily for the attractive leathery leaves.",
+      rate: 360,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/assets/images/Nertera-Granadensis-1.jpg",
+      proname: "Non Flowering",
+      prosoiltype:"Marshy-Soil",
+      prorate:"Coral Plant ₹290",
+      maintenance:"Difficult",
+      pronotes: "Has very particluar watering patterns.",
+      rate: 290,
+  
+    },
+    {
+         
+      proimg: "https://s3.amazonaws.com/mygardenlife.com/plant-library/full/6586_14.jpg",
+      proname: "Flowering",
+      prosoiltype:"Sandy-Soil",
+      prorate:"Lollipop Plant ₹470",
+      maintenance:"Moderate",
+      pronotes: "Displays a striking flower that resembles a lollipop.",
+      rate: 470,
+  
+    },
+    {
+        
+        proimg: "https://www.houseplantsexpert.com/assets/images/calathea_crocata.jpg",
+      proname: "Flowering",
+      prosoiltype:"Black-Soil",
+      prorate:"Pinstripe Calathea ₹400",
+      maintenance:"Easy",
+      pronotes: "A plant with striking leaves that are striped in white and green.",
+      rate: 400,
+  
+    },
+    {      
+      proimg: "https://www.houseplantsexpert.com/image-files/m_jasmine2.jpg",
+      proname: "Flowering",
+      prosoiltype:"Red-Soil",
+      prorate:"Madagascar Jasmine ₹500",
+      maintenance:"Difficult",
+      pronotes: "A flowering climbing vine displaying fragrant white flowers.",
+      rate: 500,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/ornamental_pepper_close_up.jpg",
+      proname: "Flowering",
+      prosoiltype:"Black-Soil",
+      prorate:"Ornamental Pepper ₹520",
+      maintenance:"Moderate",
+      pronotes: "A flowering pot type species grown for it's attractive fruit.",
+      rate: 520,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/assets/images/peace_lily_plant.JPG",
+      proname: "Flowering",
+      prosoiltype:"Black-Soil",
+      prorate:"Peace Lily ₹350",
+      maintenance:"Easy",
+      pronotes: "Shiny green leaves and white spathes make this attractive.",
+      rate: 350,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/croton1.jpg",
+      proname: "Non Flowering",
+      prosoiltype:"Sandy-Soil",
+      prorate:"Croton ₹210",
+      maintenance:"Easy",
+      pronotes: "Displays a variety of colours and patterns on the leaves.",
+      rate: 210,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/assets/images/calathea_makoyana1.jpg",
+      proname: "Non Flowering",
+      prosoiltype:"Clayey-Soil",
+      prorate:"Peacock Plant ₹310",
+      maintenance:"Moderate",
+      pronotes: "Also named Cathedral Windows due to the pattern on the leaves.",
+      rate: 310,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/venusflytrap-close-up.jpg",
+      proname: "Non Flowering",
+      prosoiltype:"Clayey-Soil",
+      prorate:"Venus Fly Trap ₹450",
+      maintenance:"Easy",
+      pronotes: "A carnivorous plant that traps and digests insects.",
+      rate: 450,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/scarlet-star-bromeliad.jpg",
+      proname: "Flowering",
+      prosoiltype:"Marshy-Soil",
+      prorate:"Scarlet Star ₹500",
+      maintenance:"Very Difficult",
+      pronotes: "A popular bromeliad species grown for it's green leathery foliage and bright red or orange bract.",
+      rate: 500,
+  
+    }, 
+];
+
+displaydata(prounder15data);
+  
+  //filter function 1. by Flowering/Non-Flowering
+  
+  function byproduct(){
+    var selected = document.querySelector("#filter1").value;
+    // console.log(selected)
+  
+    if(selected !== "Flowering/Non Flowering")
+    {
+      var filteredlist = prounder15data.filter( function (ele){
+      
+        return ele.proname == selected
+      
+      
+    });
+  
+    // console.log(filteredlist)
+    displaydata(filteredlist)
+    }
+    
+    else
+    {
+      displaydata(prounder15data)
+    }
+}
+
+  //filter function 2. bysoil
+  
+  function SOILTYPE(){
+    var selected = document.querySelector("#filter2").value;
+    // console.log(selected)
+  
+    if(selected !== "Soil Type")
+    {
+      var filteredlist = prounder15data.filter( function (ele){
+      
+        return ele.prosoiltype == selected
+      
+      
+    });
+  
+    console.log(filteredlist)
+    displaydata(filteredlist)
+    }
+  
+    else
+    {
+      displaydata(prounder15data)
+    }
+  
+    
+  }
+  //filter function 3. bymaintenance
+  
+  function maintenancelevel(){
+    var selected = document.querySelector("#filter3").value;
+    if(selected !== "Maintenance Level")
+    {
+      var filteredlist = prounder15data.filter( function (ele){ 
+        return ele.maintenance == selected
+    });
+    displaydata(filteredlist)
+    }
+    else
+    {
+      displaydata(prounder15data)
+    }
+  
+    
+  }
+      
+  //Applied sort function here
+  
+    function sortfn(){
+     
+      var selected = document.querySelector("#dosort").value;
+      if( selected =="HTL")
+      {
+        prounder15data.sort( function (a, b){
+         
+          return b.rate - a.rate;
+        });
+        displaydata(prounder15data)
+      }
+
+      if (selected =="LTH")
+      {
+        prounder15data.sort( function (a, b){
+         
+          return a.rate - b.rate;
+        });
+        displaydata(prounder15data)
+      }
+  
+    }
+
+   
+      
+var cartArr = JSON.parse(localStorage.getItem("cartItems")) || [];
+  
+function displaydata(data){
+
+  document.querySelector("#cardpro-box").innerHTML="";
+
+  data.forEach( function (ele) {
+
+  var card = document.createElement("div");
+  card.setAttribute("id","cardpro")
+ 
+  var imgdiv = document.createElement("div");
+  imgdiv.setAttribute("id", "imgdiv")
+ 
+  var imglogo = document.createElement("img")
+      imglogo.setAttribute("src",ele.proimg)
+      imgdiv.append(imglogo);
+  
+  var prodiv = document.createElement("div");
+ prodiv.setAttribute("id","prodiv");
+ 
+  var name = document.createElement("h3");
+       name.innerText = ele.proname;
+ 
+  var desc = document.createElement("h4");
+  desc.innerText = ele.prosoiltype;
+ 
+  var h3 = document.createElement("h3");
+  h3.innerText = ele.maintenance;
+  
+  var price = document.createElement("p");
+  price.innerText=ele.prorate;
+
+   var p = document.createElement("p");
+   p.setAttribute("id","Doit")
+   p.innerText = ele.pronotes;
+
+   var addtobag = document.createElement("button");
+   addtobag.setAttribute("id", "addtobagbtn")
+   addtobag.innerText="Add to Bag";
+   addtobag.style.cursor= "pointer";
+   addtobag.addEventListener("click", function(){
+     addtobagfn(ele)
+    });
+    
+    
+    var span1 = document.createElement("span");
+    
+    var span2 = document.createElement("span");
+    var span3 = document.createElement("span");
+
+    var span4 = document.createElement("span");
+
+    var span5 = document.createElement("span");
+
+
+ 
+ prodiv.append(name,desc,h3,price,p,addtobag,span1,span2,span3,span4,span5);
+     
+   card.append(imgdiv, prodiv);
+ 
+   document.querySelector("#cardpro-box").append(card);
+ 
+ });
+}
+
+function addtobagfn(ele){
+  cartArr.push(ele);
+  localStorage.setItem("cartItems", JSON.stringify(cartArr));
+  alert("This item is Added in your Cart")
+
+}
